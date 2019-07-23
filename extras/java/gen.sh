8a4a52af33b4d5fdad8fab45db51e99e99c9148e
schutzekatze <vladimir.nik94@gmail.com>
2019-07-23 16:15:55 -0700

    Update

--- extras/java/gen.sh
+++ extras/java/gen.sh
@@ -6,3 +6,7 @@ ln -sf $PWD/include extras/java/
 cd extras/java
 swig -java -c++ -Iinclude btl.i
 rm include
+
+java_files=$(find . -maxdepth 1 -iname "*.java" | sed "s~\(.*\)~'\1'~" | tr '\n' , | sed 's~.$~~')
+meson_build=$(cat meson.build)
+echo "$meson_build" | sed "s~java_files.*=.*\[.*\]~java_files = [$java_files]~" >meson.build
