fa7c9bd0a5f44717d3ef86d4941ff032a77a3428
schutzekatze <vladimir.nik94@gmail.com>
2019-07-30 15:52:02 -0700

    Update

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,5 +4,5 @@ cd "${MESON_SOURCE_ROOT}"
 
 files=$(scripts/get_include_files.sh)
 for file in $files; do
-    diff <(cat $file) <(clang-format -style=mozilla $file)
+    diff $file <(clang-format -style=mozilla $file)
 done
