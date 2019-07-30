5640c6a28775f8734698ba24e7a40a54899babef
schutzekatze <vladimir.nik94@gmail.com>
2019-07-30 15:48:58 -0700

    Update

--- scripts/get_include_files.sh
+++ scripts/get_include_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd "${MESON_SOURCE_ROOT}"
-files=$(find include -type f | grep "\(.*\.h$\)\|\(.*\.cpp$\)\|\(.*\.cxx$\)")
-echo $files
+
+files=$(find include -type f | grep "\(.*\.h$\)\|\(.*\.hpp$\)\|\(.*\.cpp$\)\|\(.*\.cxx$\)")
+for file in $files; do
+    echo -n "$(realpath $file) "
+done
--- scripts/get_wrapper_files.sh
+++ scripts/get_wrapper_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd "${MESON_SOURCE_ROOT}"
+
 files=$(find extras/python -type f | grep "\(.*\.h\)\|\(.*\.cpp\)\|\(.*\.cxx\)")
 files+=" "
 files+=$(find extras/java -type f | grep "\(.*\.h\)\|\(.*\.cpp\)\|\(.*\.cxx\)")
-echo $files
+for file in $files; do
+    echo -n "$(realpath $file) "
+done
