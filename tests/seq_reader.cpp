6481e4ea6c80d1d86080631131ad5a396bc35b12
schutzekatze <vladimir.nik94@gmail.com>
2019-12-26 01:08:01 -0800

    Update

--- tests/seq_reader.cpp
+++ tests/seq_reader.cpp
@@ -43,7 +43,7 @@ int main() {
     assert(i == 2);
 
     // Test SAM
-    btllib::SeqReader reader_sam("../tests/input.bam");
+    btllib::SeqReader reader_sam("../tests/input.sam");
     assert(reader_sam.get_format() == btllib::SeqReader::Format::SAM);
 
     i = 0;
