15ac64c900c4a8a0a1612ca45dd4a48526a6914e
Parham Kazemi <pkazemi3@gmail.com>
2022-03-30 14:50:59 -0700

    Add automatic version configuration for setup.py

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
 setup(name='btllib',
-      version='1.3.0',
+      version='@PROJECT_VERSION@',
       packages=['btllib'],
       url='https://github.com/bcgsc/btllib')
