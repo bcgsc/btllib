fb3d9f1fa8317ac56450495c2410e577816a40fe
Parham Kazemi <pkazemi3@gmail.com>
2022-04-11 14:58:30 -0700

    Transition to setuptools

--- wrappers/python/setup.py
+++ wrappers/python/setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='btllib',
       version='@PROJECT_VERSION@',
-      packages=['btllib'],
+      packages=find_packages(include=['btllib']),
       url='https://github.com/bcgsc/btllib')
