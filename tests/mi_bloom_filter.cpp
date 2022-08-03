20429bfc9d037ffcc5be1df56cd2b24df89ea261
MurathanGoktas <murathangoktas@gmail.com>
2022-08-03 14:23:22 -0700

    first mibf tests passed

---
+++ tests/mi_bloom_filter.cpp
@@ -0,0 +1,25 @@
+#include "btllib/mi_bloom_filter.hpp"
+
+#include "helpers.hpp"
+
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+int
+main()
+{
+  std::cerr << "Testing multi-indexed BloomFilter" << std::endl;
+  btllib::MIBloomFilter<uint8_t> mi_bf(1024 * 1024, 3, "ntHash");
+  mi_bf.insert_bv({ 1, 10, 100 });
+  mi_bf.insert_bv({ 100, 200, 300 });
+
+  mi_bf.complete_bv_insertion();
+
+  TEST_ASSERT(mi_bf.bv_contains({ 1, 10, 100 }));
+  TEST_ASSERT(mi_bf.bv_contains({ 100, 200, 300 }));
+  TEST_ASSERT(!mi_bf.bv_contains({ 1, 20, 100 }));
+
+  return 0;
+}
