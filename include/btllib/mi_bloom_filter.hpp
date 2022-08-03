c6433cd8f7f79037e91c489dca50336d2ed8d3a8
MurathanGoktas <murathangoktas@gmail.com>
2022-08-03 14:32:48 -0700

    clang-format

--- include/btllib/mi_bloom_filter.hpp
+++ include/btllib/mi_bloom_filter.hpp
@@ -13,129 +13,147 @@ template<typename T>
 class MIBloomFilter
 {
 public:
-	static const T MASK = T(1) << (sizeof(T) * 8 - 1);
-	static const T ANTI_MASK = (T)~MASK;
+  static const T MASK = T(1) << (sizeof(T) * 8 - 1);
+  static const T ANTI_MASK = (T)~MASK;
 
-	static const unsigned BLOCKSIZE = 512;
- 
-        /** Construct a dummy multi-indexed Bloom filter (e.g. as a default argument). */
-	MIBloomFilter() {}
+  static const unsigned BLOCKSIZE = 512;
 
-	/**
-	* Construct an empty Bloom filter of given size.
-	*
-	* @param bytes Filter size in bytes.
-	* @param hash_fn Name of the hash function used. Used for metadata. Optional.
-	*/
-	MIBloomFilter(size_t bytes, unsigned hash_num, std::string hash_fn = "");
+  /** Construct a dummy multi-indexed Bloom filter (e.g. as a default argument).
+   */
+  MIBloomFilter() {}
 
-	/** 
-	* Transform bit vector to interleaved bit vector and create ID array of size of interleaved bit vector.
-	* It can be called only once and bit vector insertions cannot be made after calling this function.
-	*/
-	bool complete_bv_insertion();
+  /**
+   * Construct an empty Bloom filter of given size.
+   *
+   * @param bytes Filter size in bytes.
+   * @param hash_fn Name of the hash function used. Used for metadata. Optional.
+   */
+  MIBloomFilter(size_t bytes, unsigned hash_num, std::string hash_fn = "");
 
-	/**
-	* Insert an element's hash values to bit vector.
-	*
-	* @param hashes Integer array of hash values. Array size should equal the
-	* hash_num argument used when the Bloom filter was constructed.
-	*/
-	void insert_bv(const uint64_t* hashes);
+  /**
+   * Transform bit vector to interleaved bit vector and create ID array of size
+   * of interleaved bit vector. It can be called only once and bit vector
+   * insertions cannot be made after calling this function.
+   */
+  bool complete_bv_insertion();
 
-	/**
-	* Insert an element's hash values to bit vector.
-	*
-	* @param hashes Integer vector of hash values. Array size should equal the
-	* hash_num argument used when the Bloom filter was constructed.
-	*/
-	void insert_bv(const std::vector<uint64_t>& hashes) { insert_bv(hashes.data()); }
-	
-	/**
-	* Check for presence of an element's hash values in the bit vector
-	* This function can run after bit vector insertion is completed.
-	*
-	* @param hashes Integer array of hash values. Array size should equal the
-	* hash_num argument used when the Bloom filter was constructed.
-	*/
-	bool bv_contains(const uint64_t* hashes);
-	
-	/**
-	* Check for presence of an element's hash values in the bit vector
-	* This function can run after bit vector insertion is completed.
-	*
-	* @param hashes Integer vector of hash values. Array size should equal the
-	* hash_num argument used when the Bloom filter was constructed.
-	*/
-	bool bv_contains(const std::vector<uint64_t>& hashes) { return bv_contains(hashes.data()); }
-	
-	/** Get population count, i.e. the number of 1 bits in the filter. */
-  	uint64_t get_pop_cnt();
-	
-	private:
-		size_t bytes = 0;
-		unsigned k = 0;
-		unsigned hash_num;
-		std::string hash_fn;
-	
-		sdsl::bit_vector bit_vector;
-		sdsl::bit_vector_il<BLOCKSIZE> il_bit_vector;
-		sdsl::rank_support_il<1> bv_rank_support;
-		T* id_array;
+  /**
+   * Insert an element's hash values to bit vector.
+   *
+   * @param hashes Integer array of hash values. Array size should equal the
+   * hash_num argument used when the Bloom filter was constructed.
+   */
+  void insert_bv(const uint64_t* hashes);
 
-		bool bv_insertion_completed, id_insertion_completed = false;
-};  	 
+  /**
+   * Insert an element's hash values to bit vector.
+   *
+   * @param hashes Integer vector of hash values. Array size should equal the
+   * hash_num argument used when the Bloom filter was constructed.
+   */
+  void insert_bv(const std::vector<uint64_t>& hashes)
+  {
+    insert_bv(hashes.data());
+  }
+
+  /**
+   * Check for presence of an element's hash values in the bit vector
+   * This function can run after bit vector insertion is completed.
+   *
+   * @param hashes Integer array of hash values. Array size should equal the
+   * hash_num argument used when the Bloom filter was constructed.
+   */
+  bool bv_contains(const uint64_t* hashes);
+
+  /**
+   * Check for presence of an element's hash values in the bit vector
+   * This function can run after bit vector insertion is completed.
+   *
+   * @param hashes Integer vector of hash values. Array size should equal the
+   * hash_num argument used when the Bloom filter was constructed.
+   */
+  bool bv_contains(const std::vector<uint64_t>& hashes)
+  {
+    return bv_contains(hashes.data());
+  }
+
+  /** Get population count, i.e. the number of 1 bits in the filter. */
+  uint64_t get_pop_cnt();
+
+private:
+  size_t bytes = 0;
+  unsigned k = 0;
+  unsigned hash_num;
+  std::string hash_fn;
+
+  sdsl::bit_vector bit_vector;
+  sdsl::bit_vector_il<BLOCKSIZE> il_bit_vector;
+  sdsl::rank_support_il<1> bv_rank_support;
+  T* id_array;
+
+  bool bv_insertion_completed, id_insertion_completed = false;
+};
 
 template<typename T>
-inline MIBloomFilter<T>::MIBloomFilter(size_t bytes, unsigned hash_num, std::string hash_fn):
-                        bytes(bytes),
-                        hash_num(hash_num),
-                        hash_fn(hash_fn){
-        bit_vector = sdsl::bit_vector(bytes);
+inline MIBloomFilter<T>::MIBloomFilter(size_t bytes,
+                                       unsigned hash_num,
+                                       std::string hash_fn)
+  : bytes(bytes)
+  , hash_num(hash_num)
+  , hash_fn(hash_fn)
+{
+  bit_vector = sdsl::bit_vector(bytes);
 }
 
 template<typename T>
-inline void MIBloomFilter<T>::insert_bv(const uint64_t* hashes){
-        assert(!bv_insertion_completed);
-        //check array size = hash_num
-        for (unsigned i = 0; i < hash_num; ++i) {
-                uint64_t pos = hashes[i] % bit_vector.size();
-                uint64_t* data_index = bit_vector.data() + (pos >> 6);
-                uint64_t bit_mask_value = (uint64_t)1 << (pos & 0x3F);
-                (void)(__sync_fetch_and_or(data_index, bit_mask_value) >> (pos & 0x3F) &
-                        1);
-        }
+inline void
+MIBloomFilter<T>::insert_bv(const uint64_t* hashes)
+{
+  assert(!bv_insertion_completed);
+  // check array size = hash_num
+  for (unsigned i = 0; i < hash_num; ++i) {
+    uint64_t pos = hashes[i] % bit_vector.size();
+    uint64_t* data_index = bit_vector.data() + (pos >> 6);
+    uint64_t bit_mask_value = (uint64_t)1 << (pos & 0x3F);
+    (void)(__sync_fetch_and_or(data_index, bit_mask_value) >> (pos & 0x3F) & 1);
+  }
 }
 template<typename T>
-inline bool MIBloomFilter<T>::bv_contains(const uint64_t* hashes){
-        assert(bv_insertion_completed);
-        for (unsigned i = 0; i < hash_num; i++){
-                if(il_bit_vector[hashes[i]] == 0){
-                        return false;
-                }
-        }
-        return true;
+inline bool
+MIBloomFilter<T>::bv_contains(const uint64_t* hashes)
+{
+  assert(bv_insertion_completed);
+  for (unsigned i = 0; i < hash_num; i++) {
+    if (il_bit_vector[hashes[i]] == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 template<typename T>
-inline bool MIBloomFilter<T>::complete_bv_insertion(){
-        assert(!id_insertion_completed);
-        bv_insertion_completed = true;
+inline bool
+MIBloomFilter<T>::complete_bv_insertion()
+{
+  assert(!id_insertion_completed);
+  bv_insertion_completed = true;
 
-        il_bit_vector = sdsl::bit_vector_il<BLOCKSIZE>(bit_vector);
-        bv_rank_support = sdsl::rank_support_il<1>(&il_bit_vector);
-        id_array = new T[get_pop_cnt()]();
-        return true;
+  il_bit_vector = sdsl::bit_vector_il<BLOCKSIZE>(bit_vector);
+  bv_rank_support = sdsl::rank_support_il<1>(&il_bit_vector);
+  id_array = new T[get_pop_cnt()]();
+  return true;
 }
 template<typename T>
-inline uint64_t MIBloomFilter<T>::get_pop_cnt() {
-        assert(bv_insertion_completed);
-        size_t index = il_bit_vector.size() - 1;
-        while (il_bit_vector[index] == 0) {
-                --index;
-        }
-        return bv_rank_support(index) + 1;
+inline uint64_t
+MIBloomFilter<T>::get_pop_cnt()
+{
+  assert(bv_insertion_completed);
+  size_t index = il_bit_vector.size() - 1;
+  while (il_bit_vector[index] == 0) {
+    --index;
+  }
+  return bv_rank_support(index) + 1;
 
-        return 32;
+  return 32;
 }
 
 } // namespace btllib
