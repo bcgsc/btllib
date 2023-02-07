cc29a6588096c957b1a91be78d793f9008df3d76
afshinfard <a.afshinfard@gmail.com>
2023-02-07 11:02:47 -0800

    regenrate docs

--- docs/search/variables_5.js
+++ docs/search/variables_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['trim_5fmasked_0',['TRIM_MASKED',['../structbtllib_1_1SeqReader_1_1Flag.html#a14037ad6ddb996106682815f75817289',1,'btllib::SeqReader::Flag']]]
+  ['seq_0',['SEQ',['../structbtllib_1_1Indexlr_1_1Flag.html#a1071f9e151bc7b70631263f8e3eea180',1,'btllib::Indexlr::Flag']]],
+  ['short_5fmode_1',['SHORT_MODE',['../structbtllib_1_1Indexlr_1_1Flag.html#ad901a6d96362ce4b250fa144fe9a76ef',1,'btllib::Indexlr::Flag::SHORT_MODE()'],['../structbtllib_1_1SeqReader_1_1Flag.html#ab0aba3f99ab4159205621e2242c6e18f',1,'btllib::SeqReader::Flag::SHORT_MODE()']]]
 ];
