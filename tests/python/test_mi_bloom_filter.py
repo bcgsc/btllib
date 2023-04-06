f03c9bf5860ddecdf41b21959de78cfa0a886913
MurathanGoktas <murathangoktas@gmail.com>
2023-04-06 15:50:48 -0700

    refactored python miBF test

--- tests/python/test_mi_bloom_filter.py
+++ tests/python/test_mi_bloom_filter.py
@@ -4,68 +4,77 @@ import string
 import btllib
 import unittest
 
-
 class MIBloomFilterTests(unittest.TestCase):
 
     def setUp(self):
         self.base_dir = os.path.dirname(__file__)
+        
+        ### mi_bf_1
+        self.test_hashes_1 = [[1, 10, 100], [100, 200, 300]]
+        ###########
+        
+        ### mi_bf_2
+        self.random_dna_2 = "GGTAGACACACGTCCACCCCGCTGCTCTGTGACAGGGACTAAAGAGGCGAAGATTATCGTGTGTGCCCCGTTATGGTCGAGTTCGGTCAGAGCGTCATTGCGAGTAGTCGTTTGCTTTCTCGAATTCCGAGCGATTAAGCGTGACAGTCCCAGCGAACCCACAAAACGTGATCGCAGTCCATGCGATCATACGCAAGAAGGAAGGTCCCCATACACCGACGCACCAGTTTACACGCCGTATGCATAAACGAGCTGCACAAACGAGAGTGCTTGAACTGGACCTCTAGTTCCTCTACAAAGAACAGGTTGACCTGTCGCGAAGTTGCCTTGCCTAGATGCAATGTCGGACGTATTACTTTTGCCTCAACGGCTCCTGCTTTCGCTGAAACCCAAGACAGGCAACAGTAACCGCCTTTTGAAGGCGAGTCCTTCGTCTGTGACTAACTGTGCCAAATCGTCTTCCAAACTCCTAATCCAGTTTAACTCACCAAATTATAGCCATACAGACCCTAATTTCATATCATATCACGCCATTAGCCTCTGCTAAAATTCTGTGCTCAAGGGTTTTGGTTCGCCCGAGTGATGTTGCCAATTAGGACCATCAAATGCACATGTTACAGGACTTCTTATAAATACTTTTTTCCTGGGGAGTAGCGGATCTTAATGGATGTTGCCAGCTGGTATGGAAGCTAATAGCGCCGGTGGGAGCGTAATCTGCCGTCTCCACCAACACAACGCTATCGGGTCATATTATAAGATTCCGCAATGGGGTTACTTATAGGTAGCCTTAACGATATCCGGAACTTGCGATGTACGTGCTATGCTTTAATACATACCTGGCCCAGTAGTTTTCCAATATGGGAACATCAATTGTACATCGGGCCGGGATAATCATGTCATCACGGAAGTAGCCGTAAGACAAATAATTCAAAAGAGATGTCGTTTTGCTAGTTCACGTGAAGGTGTCTCGCGCCACCTCTAAGTAAGTGGGCCGTCGAGA"
+        self.id_array_2 = {0, 1, 2, 3}
+        ###########
+   
+    def set_up_mi_bf_1(self):
+        self.mi_bf_1 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
+        for h in self.test_hashes_1:
+            self.mi_bf_1.insert_bv(h)
+        self.mi_bf_1.complete_bv_insertion()
 
-    def test_mibloomfilter_bv_and_id_insertion(self):
-        mi_bf_1 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
-        mi_bf_1.insert_bv([1, 10, 100])
-        mi_bf_1.insert_bv([100, 200, 300])
-        mi_bf_1.complete_bv_insertion()
+    def set_up_mi_bf_2(self):
+        self.mi_bf_2 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
+        
+        nthash = btllib.NtHash(self.random_dna_2, 1, 15)
+        while nthash.roll():
+            self.mi_bf_2.insert_bv(nthash.hashes())
+        self.mi_bf_2.complete_bv_insertion()
+        
+        for id in self.id_array_2:
+            nthash = btllib.NtHash(self.random_dna_2, 1, 15)
+            while nthash.roll():
+                self.mi_bf_2.insert_id(nthash.hashes(), id)
+         
+    def test_mibloomfilter_bv_insertion(self):
+        
+        self.set_up_mi_bf_1() 
 
-        self.assertTrue(mi_bf_1.bv_contains([1, 10, 100]))
-        self.assertTrue(mi_bf_1.bv_contains([100, 200, 300]))
-        self.assertFalse(mi_bf_1.bv_contains([1, 20, 100]))
+        self.assertTrue(self.mi_bf_1.bv_contains([1, 10, 100]))
+        self.assertTrue(self.mi_bf_1.bv_contains([100, 200, 300]))
+        self.assertFalse(self.mi_bf_1.bv_contains([1, 20, 100]))
 
-        ID_1 = 12
-        mi_bf_1.insert_id([1, 10, 100], ID_1)
+    def test_mibloomfilter_id_insertion(self):
 
-        results_1 = mi_bf_1.get_id([1, 10, 100])
-        for id in results_1:
-            self.assertEqual(id, ID_1)
+        self.set_up_mi_bf_1()
+        
+        expected_id = 12
+        self.mi_bf_1.insert_id(self.test_hashes_1[0], expected_id)
 
-        print("multi-indexed BloomFilter ID count test")
-        print("Testing ID counting")
+        for result_id in self.mi_bf_1.get_id(self.test_hashes_1[0]):
+            self.assertEqual(result_id, expected_id)
+            
+    def test_mibloomfilter_id_occurence(self):
+        self.set_up_mi_bf_1()
+        
+        expected_id = 12
+        self.mi_bf_1.insert_id(self.test_hashes_1[0], expected_id)
+
         include_saturated = True
-
-        ID_count = mi_bf_1.get_id_occurence_count(include_saturated)
-        self.assertEqual(ID_count[ID_1], 3)
+        self.assertEqual(len(self.test_hashes_1[0]), 
+                         self.mi_bf_1.get_id_occurence_count(include_saturated)[expected_id])
 
     def test_mibloomfilter_random_sampling(self):
-        print("Testing multi-indexed BloomFilter random sampling")
         
-        random_dna = "GGTAGACACACGTCCACCCCGCTGCTCTGTGACAGGGACTAAAGAGGCGAAGATTATCGTGTGTGCCCCGTTATGGTCGAGTTCGGTCAGAGCGTCATTGCGAGTAGTCGTTTGCTTTCTCGAATTCCGAGCGATTAAGCGTGACAGTCCCAGCGAACCCACAAAACGTGATCGCAGTCCATGCGATCATACGCAAGAAGGAAGGTCCCCATACACCGACGCACCAGTTTACACGCCGTATGCATAAACGAGCTGCACAAACGAGAGTGCTTGAACTGGACCTCTAGTTCCTCTACAAAGAACAGGTTGACCTGTCGCGAAGTTGCCTTGCCTAGATGCAATGTCGGACGTATTACTTTTGCCTCAACGGCTCCTGCTTTCGCTGAAACCCAAGACAGGCAACAGTAACCGCCTTTTGAAGGCGAGTCCTTCGTCTGTGACTAACTGTGCCAAATCGTCTTCCAAACTCCTAATCCAGTTTAACTCACCAAATTATAGCCATACAGACCCTAATTTCATATCATATCACGCCATTAGCCTCTGCTAAAATTCTGTGCTCAAGGGTTTTGGTTCGCCCGAGTGATGTTGCCAATTAGGACCATCAAATGCACATGTTACAGGACTTCTTATAAATACTTTTTTCCTGGGGAGTAGCGGATCTTAATGGATGTTGCCAGCTGGTATGGAAGCTAATAGCGCCGGTGGGAGCGTAATCTGCCGTCTCCACCAACACAACGCTATCGGGTCATATTATAAGATTCCGCAATGGGGTTACTTATAGGTAGCCTTAACGATATCCGGAACTTGCGATGTACGTGCTATGCTTTAATACATACCTGGCCCAGTAGTTTTCCAATATGGGAACATCAATTGTACATCGGGCCGGGATAATCATGTCATCACGGAAGTAGCCGTAAGACAAATAATTCAAAAGAGATGTCGTTTTGCTAGTTCACGTGAAGGTGTCTCGCGCCACCTCTAAGTAAGTGGGCCGTCGAGA"
-        dna_length = len(random_dna)
-        expected_id_count = dna_length // 4
-        tolerance = 0.1
+        self.set_up_mi_bf_2()
 
-        mi_bf = btllib.MIBloomFilter8(1024 * 1024, 1, "ntHash")
-        nthash = btllib.NtHash(random_dna, 1, 15)
-        while nthash.roll():
-            mi_bf.insert_bv(nthash.hashes())
+        id_occurences = self.mi_bf_2.get_id_occurence_count(True)[0:len(self.id_array_2)] ## function returns fixed size large array with zeroes
+        
+        total_id_occurences = sum(id_occurences)
+        expected_id_occurence = total_id_occurences // len(self.id_array_2) 
 
-        mi_bf.complete_bv_insertion()
-
-        ID_array = {0, 1, 2, 3}
-        for id in ID_array:
-            nthash = btllib.NtHash(random_dna, 1, 15)
-            while nthash.roll():
-                mi_bf.insert_id(nthash.hashes(), id)
-
-        results_2 = [0] * 1
-        total_counter = [0] * 4
-
-        nthash = btllib.NtHash(random_dna, 1, 15)
-        while nthash.roll():
-            results_2 = mi_bf.get_id(nthash.hashes())
-            for res in results_2:
-                total_counter[res] += 1
-
-        for count in total_counter:
-            assert count < expected_id_count + (expected_id_count * tolerance)
-            assert count > expected_id_count - (expected_id_count * tolerance)
-
-        assert mi_bf.get_pop_saturated_cnt() == 0
+        for occurence in id_occurences:
+            self.assertAlmostEqual(occurence, 
+                                   expected_id_occurence,
+                                   delta = expected_id_occurence // 10)
