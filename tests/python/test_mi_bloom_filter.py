b66b2f422594d361d16287678dfea0f833907a8a
MurathanGoktas <murathangoktas@gmail.com>
2023-04-06 18:19:02 -0700

    update miBF wrapper test

--- tests/python/test_mi_bloom_filter.py
+++ tests/python/test_mi_bloom_filter.py
@@ -15,16 +15,25 @@ class MIBloomFilterTests(unittest.TestCase):
         
         ### mi_bf_2
         self.random_dna_2 = "GGTAGACACACGTCCACCCCGCTGCTCTGTGACAGGGACTAAAGAGGCGAAGATTATCGTGTGTGCCCCGTTATGGTCGAGTTCGGTCAGAGCGTCATTGCGAGTAGTCGTTTGCTTTCTCGAATTCCGAGCGATTAAGCGTGACAGTCCCAGCGAACCCACAAAACGTGATCGCAGTCCATGCGATCATACGCAAGAAGGAAGGTCCCCATACACCGACGCACCAGTTTACACGCCGTATGCATAAACGAGCTGCACAAACGAGAGTGCTTGAACTGGACCTCTAGTTCCTCTACAAAGAACAGGTTGACCTGTCGCGAAGTTGCCTTGCCTAGATGCAATGTCGGACGTATTACTTTTGCCTCAACGGCTCCTGCTTTCGCTGAAACCCAAGACAGGCAACAGTAACCGCCTTTTGAAGGCGAGTCCTTCGTCTGTGACTAACTGTGCCAAATCGTCTTCCAAACTCCTAATCCAGTTTAACTCACCAAATTATAGCCATACAGACCCTAATTTCATATCATATCACGCCATTAGCCTCTGCTAAAATTCTGTGCTCAAGGGTTTTGGTTCGCCCGAGTGATGTTGCCAATTAGGACCATCAAATGCACATGTTACAGGACTTCTTATAAATACTTTTTTCCTGGGGAGTAGCGGATCTTAATGGATGTTGCCAGCTGGTATGGAAGCTAATAGCGCCGGTGGGAGCGTAATCTGCCGTCTCCACCAACACAACGCTATCGGGTCATATTATAAGATTCCGCAATGGGGTTACTTATAGGTAGCCTTAACGATATCCGGAACTTGCGATGTACGTGCTATGCTTTAATACATACCTGGCCCAGTAGTTTTCCAATATGGGAACATCAATTGTACATCGGGCCGGGATAATCATGTCATCACGGAAGTAGCCGTAAGACAAATAATTCAAAAGAGATGTCGTTTTGCTAGTTCACGTGAAGGTGTCTCGCGCCACCTCTAAGTAAGTGGGCCGTCGAGA"
-        self.id_array_2 = {0, 1, 2, 3}
+        self.id_array_2 = [1, 2, 3, 4]
         ###########
+        
+        ### mi_bf_3
+        self.test_hashes_3 = [[1, 10, 100], [100, 200, 300], [500, 1000, 2000]]
+        self.id_array_3 = [1, 2, 3, 4] 
+        ########### 
+                
    
     def set_up_mi_bf_1(self):
+        
         self.mi_bf_1 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
+        
         for h in self.test_hashes_1:
             self.mi_bf_1.insert_bv(h)
         self.mi_bf_1.complete_bv_insertion()
 
     def set_up_mi_bf_2(self):
+        
         self.mi_bf_2 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
         
         nthash = btllib.NtHash(self.random_dna_2, 3, 15)
@@ -36,7 +45,24 @@ class MIBloomFilterTests(unittest.TestCase):
             nthash = btllib.NtHash(self.random_dna_2, 3, 15)
             while nthash.roll():
                 self.mi_bf_2.insert_id(nthash.hashes(), id)
-         
+                
+    def set_up_mi_bf_3(self):
+        
+        self.mi_bf_3 = btllib.MIBloomFilter8(1024 * 1024, 3, "ntHash")
+        
+        for h in self.test_hashes_3:
+            self.mi_bf_3.insert_bv(h)
+        self.mi_bf_3.complete_bv_insertion()
+        
+        for id in self.id_array_3:
+            for h in self.test_hashes_3:
+                self.mi_bf_3.insert_id(h, id)
+        self.mi_bf_3.complete_id_insertion()
+        
+        for h in self.test_hashes_3:
+            for id in self.id_array_3:
+                self.mi_bf_3.insert_saturation(h, id)
+        
     def test_mibloomfilter_bv_insertion(self):
         
         self.set_up_mi_bf_1() 
@@ -69,13 +95,12 @@ class MIBloomFilterTests(unittest.TestCase):
         
         self.set_up_mi_bf_2()
 
-        id_occurences = self.mi_bf_2.get_id_occurence_count(False)[0:len(self.id_array_2)] ## function returns fixed size large array with zeroes
-        
+        id_occurences = self.mi_bf_2.get_id_occurence_count(False) ## function returns fixed size large array with zeroes
         total_id_occurences = sum(id_occurences)
         expected_id_occurence = total_id_occurences // len(self.id_array_2) 
 
-        for occurence in id_occurences:
-            self.assertAlmostEqual(occurence, 
+        for id in self.id_array_2:
+            self.assertAlmostEqual(id_occurences[id], 
                                    expected_id_occurence,
                                    delta = expected_id_occurence // 10)
             
@@ -91,8 +116,23 @@ class MIBloomFilterTests(unittest.TestCase):
             expected_id_occurences = self.mi_bf_2.get_id_occurence_count(True)
             id_occurences_loaded = loaded_mi_bf.get_id_occurence_count(True)
             
-            for i in range(len(self.id_array_2)):
+            for i in range(len(expected_id_occurences)):
                 self.assertEqual(id_occurences_loaded[i], expected_id_occurences[i])
         finally:
             os.remove(file_path)
             os.remove(file_path+".sdsl")
+            
+    def test_mibloomfilter_saturation(self):
+        
+        self.set_up_mi_bf_3()
+        
+        id_found = {i:False for i in self.id_array_3}
+        
+        results =  self.mi_bf_3.get_id(self.test_hashes_3[0]);
+        
+        for res in results:
+            self.assertTrue(res > self.mi_bf_3.MASK)
+            id_found[res & self.mi_bf_3.ANTI_MASK] = True
+
+        self.assertEqual(3,
+                         sum(value == True for value in id_found.values()))
