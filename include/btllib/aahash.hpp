704fdd38c2e2f9b9e9176966099619774b412af0
jowong04 <jowong@hpcg02.hpc.bcgsc.ca>
2023-04-11 23:39:04 -0700

    add seedAAHash

--- include/btllib/aahash.hpp
+++ include/btllib/aahash.hpp
@@ -37,11 +37,43 @@ aahash_roll(uint64_t hash_value,
   return hash_value;
 }
 
+inline uint64_t
+aa_modify_base_with_seed(uint64_t hash_value,
+                         const SpacedSeed& seed,
+                         const char* kmer_seq,
+                         const unsigned k)
+{
+  for (unsigned i = 0; i < k; i++) {
+    if (seed[i] != 1) {
+      hash_value ^=
+        (LEVEL_X_AA_SEED_LEFT_31BITS_ROLL_TABLE[1][(unsigned char)kmer_seq[i]]
+                                               [(k - 1 - i) % 31] | // NOLINT
+         LEVEL_X_AA_SEED_RIGHT_33BITS_ROLL_TABLE[1][(unsigned char)kmer_seq[i]]
+                                                [(k - 1 - i) % 33]); // NOLINT
+      if (seed[i] != 0) {
+        unsigned level = seed[i];
+        hash_value ^=
+          (LEVEL_X_AA_SEED_LEFT_31BITS_ROLL_TABLE[level][(
+             unsigned char)kmer_seq[i]][(k - 1 - i) % 31] | // NOLINT
+           LEVEL_X_AA_SEED_RIGHT_33BITS_ROLL_TABLE[level][(
+             unsigned char)kmer_seq[i]][(k - 1 - i) % 33]); // NOLINT
+      }
+    }
+  }
+  return hash_value;
+}
+
+std::vector<SpacedSeed>
+aa_parse_seeds(const std::vector<std::string>& seeds);
+class seedAAHash;
+
 class AAHash
 {
   static constexpr const char* HASH_FN_NAME = "aahash1";
 
 private:
+  friend class seedAAHash;
+
   /** Initialize internal state of iterator */
   bool init();
 
@@ -116,6 +148,103 @@ public:
   unsigned get_hash_num() const { return hash_num; }
   unsigned get_k() const { return k; }
   uint64_t get_forward_hash() const { return hashes_array[0]; }
+};
+
+class seedAAHash
+{
+private:
+  AAHash aahash;
+  const unsigned hash_num_per_seed;
+  std::unique_ptr<uint64_t[]> hashes_array;
+  std::vector<SpacedSeed> seeds;
+
+public:
+  /**
+   * Constructor.
+   * @param seq String of DNA sequence to be hashed.
+   * @param seeds Multi-level seeds to use for hashing.
+   * @param hash_num_per_seed Number of hashes to produce per seed.
+   * @param k K-mer size.
+   * @param pos Position in seq to start hashing from.
+   */
+  seedAAHash(const char* seq,
+             const std::vector<SpacedSeed>& seeds,
+             unsigned hash_num_per_seed,
+             unsigned k,
+             size_t pos = 0)
+    : aahash(seq, 1, k, 1, pos)
+    , hash_num_per_seed(hash_num_per_seed)
+    , hashes_array(new uint64_t[hash_num_per_seed * seeds.size()])
+    , seeds(seeds)
+  {
+  }
+  seedAAHash(const std::string& seq,
+             const std::vector<SpacedSeed>& seeds,
+             unsigned hash_num_per_seed,
+             unsigned k,
+             size_t pos = 0)
+    : aahash(seq, 1, k, 1, pos)
+    , hash_num_per_seed(hash_num_per_seed)
+    , hashes_array(new uint64_t[hash_num_per_seed * seeds.size()])
+    , seeds(seeds)
+  {
+  }
+  seedAAHash(const char* seq,
+             const std::vector<std::string>& seeds,
+             unsigned hash_num_per_seed,
+             unsigned k,
+             size_t pos = 0)
+    : aahash(seq, 1, k, 1, pos)
+    , hash_num_per_seed(hash_num_per_seed)
+    , hashes_array(new uint64_t[hash_num_per_seed * seeds.size()])
+    , seeds(aa_parse_seeds(seeds))
+  {
+  }
+  seedAAHash(const std::string& seq,
+             const std::vector<std::string>& seeds,
+             unsigned hash_num_per_seed,
+             unsigned k,
+             size_t pos = 0)
+    : aahash(seq, 1, k, 1, pos)
+    , hash_num_per_seed(hash_num_per_seed)
+    , hashes_array(new uint64_t[hash_num_per_seed * seeds.size()])
+    , seeds(aa_parse_seeds(seeds))
+  {
+  }
+
+  seedAAHash(const seedAAHash& seed_aahash)
+    : aahash(seed_aahash.aahash)
+    , hash_num_per_seed(seed_aahash.hash_num_per_seed)
+    , hashes_array(new uint64_t[hash_num_per_seed * seed_aahash.seeds.size()])
+    , seeds(seed_aahash.seeds)
+  {
+    std::memcpy(hashes_array.get(),
+                seed_aahash.hashes_array.get(),
+                hash_num_per_seed * seeds.size() * sizeof(uint64_t));
+  }
+  seedAAHash(seedAAHash&&) = default;
+
+  /**
+   * Calculate the hash values of current k-mer based on the seed(s) and advance
+   * to the next. seedAAHash advances one amino acid at a time until it finds a
+   * k-mer with valid characters and skips over those with invalid characters.
+   * This method must be called before hashes() is accessed, for
+   * the first and every subsequent hashed kmer. get_pos() may be called at any
+   * time to obtain the position of last hashed k-mer or the k-mer to be hashed
+   * if roll() has never been called on this AAHash object. It is important to
+   * note that the number of roll() calls is NOT necessarily equal to get_pos(),
+   * if there are N or invalid characters in the hashed sequence.
+   *
+   * @return true on success and false otherwise.
+   */
+  bool roll();
+
+  const uint64_t* hashes() const { return hashes_array.get(); }
+
+  size_t get_pos() const { return aahash.get_pos(); }
+  unsigned get_hash_num() const { return aahash.get_hash_num(); }
+  unsigned get_hash_num_per_seed() const { return hash_num_per_seed; }
+  unsigned get_k() const { return aahash.get_k(); }
 };
 
 } // namespace btllib
--- src/btllib/aahash.cpp
+++ src/btllib/aahash.cpp
@@ -3,6 +3,21 @@
 #include "btllib/nthash_lowlevel.hpp"
 
 namespace btllib {
+std::vector<SpacedSeed>
+aa_parse_seeds(const std::vector<std::string>& seeds)
+{
+  std::vector<SpacedSeed> seed_vec;
+  // convert vector of string to vector of vector of unsigned
+  for (const auto& seed : seeds) {
+    SpacedSeed seed_vec_tmp;
+    for (const auto& c : seed) {
+      // push back the unsigned value of the char by type conversion
+      seed_vec_tmp.push_back((unsigned)(c - '0'));
+    }
+    seed_vec.push_back(seed_vec_tmp);
+  }
+  return seed_vec;
+}
 
 bool
 AAHash::init()
@@ -41,4 +56,22 @@ AAHash::roll()
   return true;
 }
 
+bool
+seedAAHash::roll()
+{
+  if (!aahash.roll()) {
+    return false;
+  }
+  uint64_t hash_value = aahash.hashes_array[0];
+  for (size_t i = 0; i < seeds.size(); ++i) {
+    hashes_array[i * hash_num_per_seed] = aa_modify_base_with_seed(
+      hash_value, seeds[i], aahash.seq + aahash.pos, aahash.k);
+    nte64(hash_value,
+          aahash.k,
+          hash_num_per_seed,
+          hashes_array.get() + i * hash_num_per_seed);
+  }
+
+  return true;
+}
 } // namespace btllib
