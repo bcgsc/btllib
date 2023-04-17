4633a39f216ae61b85fdb35a50e89a71a6ea488c
MurathanGoktas <murathangoktas@gmail.com>
2023-04-17 16:53:39 -0700

    fasta seq reader python test added

--- tests/python/test_seq_reader.py
+++ tests/python/test_seq_reader.py
@@ -14,3 +14,20 @@ class SeqReaderTests(unittest.TestCase):
         with btllib.SeqReader(path, btllib.SeqReaderFlag.SHORT_MODE) as reader:
             read = [record.seq for record in reader]
         self.assertListEqual([seq], read)
+        
+    def test_fasta(self):
+        ids = ["asdf", "ghjk"]
+        seqs = ["ACTG", "TGCA"]
+
+        for iteration in range(3):
+            reader = btllib.SeqReader(os.path.join(self.base_dir, "../input.fa.gz.bz2.xz"),
+                                      btllib.SeqReaderFlag.SHORT_MODE)
+            self.assertEqual(reader.get_format(), btllib.SeqReader.SeqReaderFormat_FASTA)
+
+            i = 0
+            for record in reader:
+                self.assertEqual(record.id, ids[i])
+                self.assertEqual(record.seq, seqs[i])
+                self.assertEqual(record.qual, "")
+                i += 1
+            self.assertEqual(i, 2)
