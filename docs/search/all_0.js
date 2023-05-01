8299659c7a9485a9eae67252ad2729a285dc14ff
jowong04 <jowong@hpcg02.hpc.bcgsc.ca>
2023-04-30 21:41:33 -0700

    regenerated docs

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['aahash_0',['AAHash',['../classbtllib_1_1AAHash.html#a8a957a6a17d158e4b782d5459a470811',1,'btllib::AAHash::AAHash()'],['../classbtllib_1_1AAHash.html',1,'btllib::AAHash']]]
+  ['aahash_0',['AAHash',['../classbtllib_1_1AAHash.html#a1b34e66b73d76625fe21f94473bf9d36',1,'btllib::AAHash::AAHash()'],['../classbtllib_1_1AAHash.html',1,'btllib::AAHash']]]
 ];
--- docs/search/classes_a.js
+++ docs/search/classes_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['seedaahash_0',['seedAAHash',['../classbtllib_1_1seedAAHash.html',1,'btllib']]],
+  ['seedaahash_0',['SeedAAHash',['../classbtllib_1_1SeedAAHash.html',1,'btllib']]],
   ['seedbloomfilter_1',['SeedBloomFilter',['../classbtllib_1_1SeedBloomFilter.html',1,'btllib']]],
   ['seednthash_2',['SeedNtHash',['../classbtllib_1_1SeedNtHash.html',1,'btllib']]],
   ['seqreader_3',['SeqReader',['../classbtllib_1_1SeqReader.html',1,'btllib']]],
