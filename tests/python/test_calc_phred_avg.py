6c7fba26696e2909473fc661f5c18abef399752c
jowong04 <jowong@hpce704.hpc.bcgsc.ca>
2023-07-05 15:33:51 -0700

    calc_phred_avg: wrapper tests

---
+++ tests/python/test_calc_phred_avg.py
@@ -0,0 +1,16 @@
+import os
+import btllib
+import unittest
+
+
+class CalcPhredAvgTests(unittest.TestCase):
+
+    def test_calc_phred_avg(self):
+        qual = "$$%%)*0)'%%&$$%&$&'''*)(((((()55561--.12356577-++**++,////.*))((()+))**010/..--+**++*+++)++++78883"
+        self.assertAlmostEqual(
+            btllib.calc_phred_avg(qual, 0, 10), 6.4, places=3)
+        self.assertAlmostEqual(btllib.calc_phred_avg(qual), 10.949, places=3)
+        self.assertAlmostEqual(
+            btllib.calc_phred_avg(qual, 0, 4), 3.5, places=3)
+        self.assertAlmostEqual(
+            btllib.calc_phred_avg(qual, 5, 20), 6.15, places=3)
